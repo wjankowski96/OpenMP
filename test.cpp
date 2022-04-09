180619af0f75fcfa8f766a59f7f7152368367c49
Wojciech Jankowski <48632535+wjankowski96@users.noreply.github.com>
2022-04-09 22:39:52 +0200

    simpson działa

--- test.cpp
+++ test.cpp
@@ -24,22 +24,21 @@ float calkowanie(float poczPrzedzial, float koncPrzedzial, int n, int red)
     int lobliczen = n, lwatkow = red;
     double h;
     /* Zapewnienie dostepu do innego bloku pamieci dla kazdego watku*/
-     #pragma omp parallel private(xs) //wątek powinien mieć własne wystąpienie zmiennej
+  //wątek powinien mieć własne wystąpienie zmiennej
         h = (b - a) / (int)lobliczen;            //odleglosc miedzy dwoma sasiednimi punktami, przedzial calkowania dzielony na N rownych czesci
-        //#pragma omp for num_threads(lwatkow) reduction(+: calka)//zmiennea, które jest prywatne dla każdego wątku jest obiektem operacji redukcji na końcu regionu równoległego
-        for (int i = 1; i < lobliczen; i++)
-        {
-            xs = a + i * h;                //pozycja punktu przedziałowego
-            s +=func (xs - h / 2);         //punkt srodkowy dla ktorego bedzie obliczane przyblizenie               
-            calka += func(xs);
-        }
+    #pragma omp parallel for num_threads(lwatkow) reduction(+: s,calka) private(xs)//zmiennea, które jest prywatne dla każdego wątku jest obiektem operacji redukcji na końcu regionu równoległego
+    for (int i = 1; i < lobliczen; i++)
+    {
+        xs = a + i * h;                //pozycja punktu przedziałowego
+        s +=func (xs - h / 2);         //punkt srodkowy dla ktorego bedzie obliczane przyblizenie               
+        calka += func(xs);
+    }
         s += func(b - h / 2);           //suma wartości funkcji w punktach srodkowych
         /* Konstrukcja critical ogranicza wykonanie powiązanego z nią bloku strukturalnego w jednym wątku w danym czasie */
         //#pragma omp critical
         calka = (h / 6) * (func(a) + func(b) + 2 * calka + 4 * s); //prsyblizona wartosc calki oznaczonej funckji f(x) w przedziale pocz i konc
-
+    
     printf("Czas Simpsonem wynosi %f\n", omp_get_wtime() - now) ;
-        
     
     
     return calka;
@@ -53,14 +52,32 @@ float trapezy(float poczPrzedzial, float koncPrzedzial, int n, int red){
     int lwatkow=red;
     float dx = (koncPrzedzial - poczPrzedzial) / liczbaObli;
     double now = omp_get_wtime();
-    
-    #pragma omp parallel for num_threads(lwatkow) reduction(+: s) 
-     for(int i = 1; i < liczbaObli; i++) {
-         s += func(poczPrzedzial + i * dx);
-         }
-  wynik = (s + (func(poczPrzedzial) + func(koncPrzedzial)) / 2) * dx;
+    const int N = static_cast<int>((koncPrzedzial - poczPrzedzial) / dx);
+
+  //#pragma omp parallel for num_threads(lwatkow) reduction(+: s)
+    for(int i = 1; i < N; i++) {
+        s += func(poczPrzedzial + i * dx);
+        }
+  wynik += (s + (func(poczPrzedzial) + func(koncPrzedzial)) / 2) * dx;
   printf("Czas trapezowym wynosi %f\n", omp_get_wtime() - now) ;
   return wynik;
+}
+
+
+float trapezy1(float poczPrzedzial, float koncPrzedzial, double n, int red){
+const int N = static_cast<int>((koncPrzedzial - poczPrzedzial) / n);
+	double now = omp_get_wtime();
+	double s = 0;
+
+	#pragma omp parallel for num_threads(red) reduction(+: s)
+	for (int i = 1; i < N; i++) 
+    s += func(poczPrzedzial + i * n);
+
+	double wynik = (s + (func(poczPrzedzial) + func(koncPrzedzial)) / 2) * n;
+	 
+	printf("Czas trapezowym wynosi %f\n", omp_get_wtime() - now);
+    return wynik;
+
 }
 
 float calkowaniebez(float poczPrzedzial, float koncPrzedzial, int n, int red)
@@ -94,9 +111,9 @@ float calkowaniebez(float poczPrzedzial, float koncPrzedzial, int n, int red)
 /* Program glowny w ktorym wywolywana jest funkcja obliczajaca calke i pobierane sa wartosci potrzbene do okreslenia poczatku i konca, ilosci watkow i przedzialu calkowania */
 int main()
 {
-    double poczPrzedzial, koncPrzedzial, dx, calkat, s, x, A, B, C;
+    double poczPrzedzial, koncPrzedzial, dx, calkat, s, x, A, B, C,n;
      double A1;
-    int i, n, red;
+    int i,  red;
     printf("Obliczamy calke dla funkcji Ax^2+Bx+C\n");
     
    //printf("Podaj zmienna A\n");
@@ -109,7 +126,7 @@ int main()
     scanf("%lf", &koncPrzedzial);
 
     printf("Podaj liczbe przedzialow\n");
-    scanf("%d", &n);
+    scanf("%lf", &n);
 
     printf("Podaj ilosc watkow\n");
     scanf("%d", &red);
@@ -120,13 +137,16 @@ int main()
 
     double wynik1 = trapezy(poczPrzedzial, koncPrzedzial, n, red);
     printf("Wartosc calki metoda trapezow wynosi=  %f\n",wynik1);
+
+    double wynik3 = trapezy1(poczPrzedzial, koncPrzedzial, n, red);
+    printf("Wartosc calki metoda trapezow2 wynosi=  %f\n",wynik3);
+
  double wynik2 = calkowaniebez(poczPrzedzial, koncPrzedzial, n, red);
     printf("Wartosc calki metoda trapezow wynosi=  %f\n",wynik2);
 
     return 0;
  
 }
-
 
 
 
