0e47f68d9fa54ea9171e42fd60227b2cf018c7c1
Wojciech Jankowski <48632535+wjankowski96@users.noreply.github.com>
2022-04-24 12:10:45 +0200

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,4 +1,5 @@
- #include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
 #include <omp.h>
 #include <iostream>
 #include <fstream>
@@ -18,175 +19,119 @@ using namespace std;
 #include <vector>
 #include <sstream>
 #include <iostream>
-using namespace std;
-FILE * myFile3 = stdout;
-#include <stdio.h>
-//#include <process.h>
+
+#include <stdlib.h>
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <sstream>
+
 int main()
 {
 
-
 /*
-    WCZYTANIE PIERWSZEJ MACIERZY
+POBRANIE MACIERZY PIERWSZEJ
 */
-string fname;
-	cout<<"Enter the file name: ";
-	cin>>fname;
- 
-	vector< vector<string> > content;
-	vector<string> row;
-	string line, word;
- 
-	fstream file (fname, ios::in);
-	if(file.is_open())
+  vector<double> matrix;
+  //readfile
+  fstream file;
+  file.open("matrix1.csv");
+  string line;
+  while (getline( file, line,'\n'))  //讀檔讀到跳行字元
 	{
-		while(getline(file, line))
-		{
-			row.clear();
- 
-			stringstream str(line);
- 
-			while(getline(str, word, ';'))
-				row.push_back(word);
-			content.push_back(row);
-		}
+	  istringstream templine(line); // string 轉換成 stream
+	  string data;
+	  while (getline( templine, data,';')) //讀檔讀到逗號
+	  {
+	    matrix.push_back(atof(data.c_str()));  //string 轉換成數字
+	  }
 	}
-	else
-		cout<<"Could not open the file\n";
-
- double matrix1[content.size()][content.size()];
-	for(int i=0;i<content.size();i++)
-	{
-		for(int j=0;j<content[i].size();j++)
-		{
-			matrix1[i][j]=(int)content[i][j];
-		}
-		cout<<"\n";
-	}
-
-/*
-    WCZYTANIE DRUGIEJ MACIERZY
-
-string fname1;
-	cout<<"Enter the file name: ";
-	cin>>fname1;
- 
-	vector< vector<string> > content1;
-	vector<string> row1;
-	string line1, word1;
- 
-	fstream file1 (fname1, ios::in);
-	if(file1.is_open())
-	{
-		while(getline(file1, line1))
-		{
-			row.clear();
- 
-			stringstream str(line1);
- 
-			while(getline(str, word1, ';'))
-				row.push_back(word1);
-			content.push_back(row1);
-		}
-	}
-	else
-		cout<<"Could not open the file\n";
-
- double matrix2[content1.size()][content1.size()];
-	for(int i=0;i<content1.size();i++)
-	{
-		for(int j=0;j<content1[i].size();j++)
-		{
-			matrix2[i][j]=stod(content1[i][j]);
-		}
-		cout<<"\n";
-	}
-
-
-*/
-
-
-    /*
-    int a[10][10], b[10][10], mult[10][10], r1, c1, r2, c2, i, j, k;
-
-    cout << "Wprowadz liczbe wierszy i kolumn pierwszej macierzy: ";
-    cin >> r1 >> c1;
-    cout << "Wprowadz liczbe wierszy i kolumn drugiej macierzy: ";
-    cin >> r2 >> c2;
-
-    
-    while (c1!=r2)
-    {
-        cout << "Blad, ilosci kolumn pierwszej nie jest rowna drugiej wierszy.";
-
-        cout << "Wprowadz liczbe  kolumn i wierszy pierwszej macierzy ";
-        cin >> r1 >> c1;
-
-        cout << "EnWprowadz liczbe  kolumn i wierszy drugiej macierzy: ";
-        cin >> r2 >> c2;
+  file.close();
+  int row =  (line.size()/2);
+  double matrix1[row][row];
+  int a=0;
+for(int i=0; i<row; i++){
+    for(int j=0; j<row; j++){
+        
+       matrix1[i][j]  = matrix[a];
+       a++;
     }
+}
+for(int i=0; i<row; i++){
+    for(int j=0; j<row; j++){
+    cout << " " << matrix1[i][j];
+        if(j == row-1)
+            cout << endl;
+    }}
 
-    
-    cout << endl << "Wprowadz wartosci dla macierzy 1:" << endl;
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c1; ++j)
+  //writefile
+  file.open("write.csv");
+  for (int i=0;i<matrix.size();i++)
+  {
+    file << matrix[i]<<",";
+  }
+  file.close();
+cout<<endl<<endl;
+  /*
+  POBRANIE DRUGIEJ MACIERZY
+  */
+
+ vector<double> matrix2;
+  //readfile
+  fstream file1;
+  file.open("matrix2.csv");
+  string line1;
+  while (getline( file1, line1,'\n'))  //讀檔讀到跳行字元
+	{
+	  istringstream templine1(line1); // string 轉換成 stream
+	  string data1;
+	  while (getline( templine1, data1,';')) //讀檔讀到逗號
+	  {
+	    matrix2.push_back(atof(data1.c_str()));  //string 轉換成數字
+	  }
+	}
+  file1.close();
+  int row1 =  (line1.size()/2);
+  double matrix3[row1][row1];
+  int a1=0;
+for(int i=0; i<row1; i++){
+    for(int j=0; j<row1; j++){
+        
+       matrix3[i][j]  = matrix2[a1];
+       a1++;
+    }
+}
+for(int i=0; i<row1; i++){
+    for(int j=0; j<row1; j++){
+    cout << " " << matrix3[i][j];
+        if(j == row-1)
+            cout << endl;
+    }}
+cout<<endl<<endl;
+/*
+MNOZENIE MACIERZY
+*/
+double mnozenie[row1][row1];
+for(int i = 0; i < row1; ++i)
+        for(int j = 0; j < row1; ++j)
         {
-            cout << "Wartość macierzy a" << i + 1 << j + 1 << " : ";
-            cin >> a[i][j];
+            mnozenie[i][j]=0;
         }
-
-   
-    cout << endl << "Wprowadz wartosci dla macierzy 2:" << endl;
-    for(i = 0; i < r2; ++i)
-        for(j = 0; j < c2; ++j)
-        {
-            cout << "Wartość macierzy b" << i + 1 << j + 1 << " : ";
-            cin >> b[i][j];
-        }
-
-   
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c2; ++j)
-        {
-            mult[i][j]=0;
-        }
-
     // mnozenie macierzy a i b
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c2; ++j)
-            for(k = 0; k < c1; ++k)
+    for(int i = 0; i < row1; ++i)
+        for(int j = 0; j < row1; ++j)
+            for(int k = 0; k < row1; ++k)
             {
-                mult[i][j] += a[i][k] * b[k][j];
+                mnozenie[i][j] += matrix1[i][k] * matrix3[k][j];
             }
-
     // wyswietlanie wyniku mnozenia
     cout << endl << "Wynik mnozenia macierzy: " << endl;
-    for(i = 0; i < r1; ++i)
-    for(j = 0; j < c2; ++j)
+    for(int i = 0; i < row1; ++i)
+    for(int j = 0; j < row1; ++j)
     {
-        cout << " " << mult[i][j];
-        if(j == c2-1)
+        cout << " " << mnozenie[i][j];
+        if(j == row1-1)
             cout << endl;
     }
-//Zapis do pliku wynikow mnozenia
-fstream file;
-    file.open ("matrix3.csv", ios::out | ios::app);
-    if (file) {
-        file << "Liczba kolumn: "<<c1<<endl;
-        file<<"Liczba wierszy: "<<c2 <<endl;
-        for(int i = 0; i < c1; i++){
-            for(int j = 0; j < c2; j++)
-            {
-                file << mult[i][j]<<";";
-            }
-            file <<  endl;
-        }
-            
-    }else{
-        printf("Blad pliku");
-    }
-    
-    file.close();
-    */
-    return 0;
+  return 0;
 }
