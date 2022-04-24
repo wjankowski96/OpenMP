176241049b3b83c3c84b9f49d0200ab18f3bfccf
Wojciech Jankowski <48632535+wjankowski96@users.noreply.github.com>
2022-04-24 15:24:00 +0200

    dodać pragmaomp

--- Matrix.cpp
+++ Matrix.cpp
@@ -25,7 +25,8 @@ using namespace std;
 #include <string> 
 #include <fstream>
 #include <sstream>
-
+#include "stdio.h"
+#include <sys/time.h>
 int main()
 {
   vector<double> matrix;
@@ -106,8 +107,10 @@ for(int i=0; i<row1; i++){
 cout<<endl<<row1<<endl;
 
 /*
-MNOZENIE MACIERZY
+MNOZENIE MACIERZY JEDNOWĄTKOWA
 */
+ struct timeval begin, end; //structura danych typu timeval
+    gettimeofday(&begin, 0); //funckja pobiera zegar systemowy
 double mnozenie[row1][row1];
 for(int i = 0; i < row1; ++i)
         for(int j = 0; j < row1; ++j)
@@ -130,10 +133,57 @@ for(int i = 0; i < row1; ++i)
         if(j == row1-1)
             cout << endl;
     }
+    gettimeofday(&end, 0); // pobranie zegara systemowego z konca procesu ktůre up≥ynÍ≥y, i mikrosekundach od 00:00:00, 1 stycznia 1970 r.
+    long seconds = end.tv_sec - begin.tv_sec;
+    long microseconds = end.tv_usec - begin.tv_usec;
+    double elapsed = seconds + microseconds*1e-6;
+
+
+    printf("czas realizacji zadania sekwencyjnie przez jeden watek Ts: %.3f seconds.\n", elapsed);
+
+
+
+
+/*
+MNOZENIE MACIERZY WIELOWĄTKOWA
+*/
+
+struct timeval begin1, end1; //structura danych typu timeval
+    gettimeofday(&begin1, 0); //funckja pobiera zegar systemowy
+double mnozenie1[row1][row1];
+for(int i = 0; i < row1; ++i)
+        for(int j = 0; j < row1; ++j)
+        {
+            mnozenie1[i][j]=0;
+        }
+    // mnozenie macierzy a i b
+    for(int i = 0; i < row1; ++i)
+        for(int j = 0; j < row1; ++j)
+            for(int k = 0; k < row1; ++k)
+            {
+                mnozenie1[i][j] += matrix1[i][k] * matrix3[k][j];
+            }
+    // wyswietlanie wyniku mnozenia
+    cout << endl << "Wynik mnozenia macierzy: " << endl;
+    for(int i = 0; i < row1; ++i)
+    for(int j = 0; j < row1; ++j)
+    {
+        cout << " " << mnozenie1[i][j];
+        if(j == row1-1)
+            cout << endl;
+    }
+    gettimeofday(&end1, 0); // pobranie zegara systemowego z konca procesu ktůre up≥ynÍ≥y, i mikrosekundach od 00:00:00, 1 stycznia 1970 r.
+    long seconds1 = end1.tv_sec - begin1.tv_sec;
+    long microseconds1 = end1.tv_usec - begin1.tv_usec;
+    double elapsed1 = seconds1 + microseconds1*1e-6;
+
+
+    printf("czas realizacji zadania sekwencyjnie przez wielewątkow watek TP: %.3f seconds.\n", elapsed1);
 
     fstream file4;
      file4.open ("matrix3.csv", ios::out | ios::app);
      if (file4) {
+         file4<<"czas realizacji zadania sekwencyjnie przez jeden watek Ts: "<<elapsed<<endl;
          file4 << "Liczba kolumn: "<<row1<<endl;
          file4<<"Liczba wierszy: "<<row1 <<endl;
          for(int i = 0; i < row1; i++){
@@ -144,5 +194,6 @@ for(int i = 0; i < row1; ++i)
              file4 <<  endl;
          }
      }
+
   return 0;
 }
