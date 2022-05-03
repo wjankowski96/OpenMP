86d1d53216ebbfc44b61467cf5a7767d05e9fa21
Wojciech Jankowski <48632535+wjankowski96@users.noreply.github.com>
2022-05-03 12:57:55 +0200

    dziala

--- Matrix.cpp
+++ Matrix.cpp
@@ -44,7 +44,7 @@ int main()
 	}
   file.close();
   int row =  3;//(line.size()/2);
-  float matrix1[2][3];
+  float matrix1[row][row+1];
   int a=0;
   int wiersz=matrix[a];
   cout<<"liczba wierszy "<<wiersz<<endl;
@@ -64,9 +64,9 @@ for(int i=0; i<3; i++){
         if(j == 3)
             cout << endl;
     }}
-
+/*
 cout<<"-------------gauss-------------"<<endl<<endl;
-/*int i,j,k,n=3;
+int i,j,k,n=row;
 float b;
 float x[4];
 
@@ -98,8 +98,8 @@ for(j=1; j<=n; j++) {
 */
 
 
-
-int N=3,i,j,k;
+/*
+int N=row,i,j,k;
 float temp=0,s, x[N];
 for(int j=0; j<N-1; j++)
 	{
@@ -137,7 +137,94 @@ for(int j=0; j<N-1; j++)
 	for(i=0; i<N; i++)
 	 cout<<"x["<<setw(3)<<i+1<<"]="<<setw(7)<<setprecision(4)<<x[i]<<endl;
 
-	
+*/	
+
+/* kolejny sposob
+int i,j,k;
+int n=row;
+float res[n];
+ for(i=0;i<n;i++) 
+    {                   
+        for(j=i+1;j<n;j++)
+        {
+            if(abs(matrix1[i][i]) < abs(matrix1[j][i]))
+            {
+                for(k=0;k<n+1;k++)
+                {
+                    
+        matrix1[i][k]=matrix1[i][k]+matrix1[j][k];
+                    matrix1[j][k]=matrix1[i][k]-matrix1[j][k];
+                    matrix1[i][k]=matrix1[i][k]-matrix1[j][k];
+                }
+            }
+      }
+    }
+   
+     
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            float f=matrix1[j][i]/matrix1[i][i];
+            for(k=0;k<n+1;k++)
+            {
+              matrix1[j][k]=matrix1[j][k]-f*matrix1[i][k];
+      }
+        }
+    }
+    
+    for(i=n-1;i>=0;i--)          
+    {                     
+        res[i]=matrix1[i][n];
+                    
+        for(j=i+1;j<n;j++)
+        {
+          if(i!=j)
+          {
+              res[i]=res[i]-matrix1[i][j]*res[j];
+    }          
+  }
+  res[i]=res[i]/matrix1[i][i];  
+    }
+cout<<"\n The Solution is:\n";
+	for(i=0; i<n; i++)
+	 cout<<"x["<<setw(3)<<i+1<<"]="<<setw(7)<<setprecision(4)<<res[i]<<endl;
+*/
+int n=row,i,j,k;
+float x[n];
+cout<<"\nThe matrix after Pivotisation is:\n";
+    for (i=0;i<n;i++)            //print the new matrix
+    {
+        for (j=0;j<=n;j++)
+            cout<<matrix1[i][j]<<setw(16);
+        cout<<"\n";
+    }    
+    for (i=0;i<n-1;i++)            //loop to perform the gauss elimination
+        for (k=i+1;k<n;k++)
+            {
+                float t=matrix1[k][i]/matrix1[i][i];
+                for (j=0;j<=n;j++)
+                    matrix1[k][j]=matrix1[k][j]-t*matrix1[i][j];    //make the elements below the pivot elements equal to zero or elimnate the variables
+            }
+     
+    cout<<"\n\nThe matrix after gauss-elimination is as follows:\n";
+    for (i=0;i<n;i++)            //print the new matrix
+    {
+        for (j=0;j<=n;j++)
+            cout<<matrix1[i][j]<<setw(16);
+        cout<<"\n";
+    }
+    for (i=n-1;i>=0;i--)                //back-substitution
+    {                        //x is an array whose values correspond to the values of x,y,z..
+        x[i]=matrix1[i][n];                //make the variable to be calculated equal to the rhs of the last equation
+        for (j=i+1;j<n;j++)
+            if (j!=i)            //then subtract all the lhs values except the coefficient of the variable whose value                                   is being calculated
+                x[i]=x[i]-matrix1[i][j]*x[j];
+        x[i]=x[i]/matrix1[i][i];            //now finally divide the rhs by the coefficient of the variable to be calculated
+    }
+    cout<<"\nThe values of the variables are as follows:\n";
+    for (i=0;i<n;i++)
+        cout<<x[i]<<endl;            // Print the va
   
     return 0;
 }
